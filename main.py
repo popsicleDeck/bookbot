dc580823f038161d21cfc81e13dc7fac57117d62
popsicleDeck <nguyenchuminh125@gmail.com>
2025-06-25 21:21:44 +1000

    add characters counter

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from stats import words_counter
+from stats import words_counter, char_counter
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 def main():
-    text = get_book_text('books/frankenstein.txt').split()
-    print(f"{words_counter(text)} words found in the document")
+     text = get_book_text('books/frankenstein.txt').split()
+     print(f"{words_counter(text)} words found in the document")
+     num_char = char_counter(get_book_text('books/frankenstein.txt'))
+     print(num_char)
+
 main()
-
-
-
--- stats.py
+++ stats.py
@@ -1,3 +1,12 @@
 def words_counter(text):
     return len(text)
 
+def char_counter(text):
+    text = text.lower()
+    dict_ = {}
+    for char in text:
+        if char not in dict_:
+            dict_[char] = 0
+        dict_[char] += 1
+    return dict_
+
